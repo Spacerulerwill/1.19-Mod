20926d0de9aca71dd802b4e75ebcbad71ad78e24
Spacerulerwill <williamdredding@gmail.com>
2022-06-26 11:08:15 +0100

    Added full armor set and recipes

--- src/main/java/com/spacerulerwill/testingmod/init/ItemInit.java
+++ src/main/java/com/spacerulerwill/testingmod/init/ItemInit.java
@@ -10,7 +10,20 @@ import net.minecraftforge.registries.RegistryObject;
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestingMod.MOD_ID);
 
+    // gilded diamond armor
+    public static final RegistryObject<Item> GILDED_DIAMOND_HELMET = ITEMS.register("gilded_diamond_helmet", () -> new GildedArmorItem(ArmorMaterialsInit.GILDED_DIAMOND, EquipmentSlot.HEAD,
+            new Item.Properties()
+                    .tab(CreativeModeTab.TAB_COMBAT)
+    ));
     public static final RegistryObject<Item> GILDED_DIAMOND_CHESTPLATE = ITEMS.register("gilded_diamond_chestplate", () -> new GildedArmorItem(ArmorMaterialsInit.GILDED_DIAMOND, EquipmentSlot.CHEST,
+            new Item.Properties()
+                    .tab(CreativeModeTab.TAB_COMBAT)
+    ));
+    public static final RegistryObject<Item> GILDED_DIAMOND_LEGGINGS = ITEMS.register("gilded_diamond_leggings", () -> new GildedArmorItem(ArmorMaterialsInit.GILDED_DIAMOND, EquipmentSlot.LEGS,
+            new Item.Properties()
+                    .tab(CreativeModeTab.TAB_COMBAT)
+    ));
+    public static final RegistryObject<Item> GILDED_DIAMOND_BOOTS = ITEMS.register("gilded_diamond_boots", () -> new GildedArmorItem(ArmorMaterialsInit.GILDED_DIAMOND, EquipmentSlot.FEET,
             new Item.Properties()
                     .tab(CreativeModeTab.TAB_COMBAT)
     ));
